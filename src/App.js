7ffd5f46190e47985063166f95bb6e730067277a
ezure haruna <burimofumofu@gmail.com>
2021-05-02 13:12:52 +0900

    feat: 6_useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const devide3 = () =>
+    setCount((previousCount) => {
+      return previousCount % 3 === 0 ? previousCount / 3 : previousCount;
+    });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>count: {count}</div>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <button onClick={() => setCount(0)}>reset</button>
+      <button onClick={devide3}>devide3</button>
     </div>
   );
-}
+};
 
 export default App;
