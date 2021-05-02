1e242b990f5c088a9928409328a61b2f931c022a
ezure haruna <burimofumofu@gmail.com>
2021-05-02 13:58:31 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = (props) => {
   const [name, setName] = useState("サンプル");
   const [price, setPrice] = useState(1000);
+
+  useEffect(() => {
+    console.log("useEffect is invoked");
+  });
+
+  useEffect(() => {
+    console.log("This is like componentDidMount");
+  }, []);
+
+  useEffect(() => {
+    console.log("This callback is for name only");
+  }, [name]);
+
   return (
     <div>
       <p>
-        現在の{name}は、{price}円です。
+        現在の{name}は、{price}円です.
       </p>
       <button onClick={() => setPrice(price + 1)}>+1</button>
       <button onClick={() => setPrice(price - 1)}>-1</button>
